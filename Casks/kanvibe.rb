04d1ec9472a04394b06451c8a23a2075ecdd5875
rookedsysc <rookedsysc36@gmail.com>
2026-08-23 17:07:52 +0900

    Update KanVibe cask to 1.2.4

--- Casks/kanvibe.rb
+++ Casks/kanvibe.rb
@@ -1,6 +1,6 @@
 cask "kanvibe" do
-  version "1.2.3"
-  sha256 "cbdbd1e71ad31cce0e46f5778b75e257be17e5d8bcc268c10062a4a54fdf7d60"
+  version "1.2.4"
+  sha256 "0e5b75108ec8bdd40c5547b2a62c3c95c6ea210407c113c0e1b6818cc94f7028"
 
   url "https://github.com/rookedsysc/kanvibe/releases/download/#{version}/KanVibe-#{version}.dmg"
   name "KanVibe"
